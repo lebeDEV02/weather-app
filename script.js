d8a8d06a5e823ede4ca664dbea4bd3e8deaa3874
lebeDEV02 <71211538+lebeDEV02@users.noreply.github.com>
2021-03-23 10:58:37 +0300

    Added list of favourite cities

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ const weather = document.querySelector('.weather-information__weather');
 const weatherSunrise = document.querySelector('.weather-information__sunrise')
 const weatherSunset = document.querySelector('.weather-information__sunset')
 const weatherCityArray = document.querySelectorAll('.app__weather-city');
+const weatherCityFavourite = document.querySelector('.app__weather-city--favourite');
 const inputValue = document.querySelector('.app__search-input')
 const btn = document.querySelector('.app__search-button');
 const favouriteCity = document.querySelector('.weather__favourite');
 const addedLocations = document.querySelector('.app__locations-list');
 const weatherDigitsValue = document.querySelector('.weather__value');
+let set = new Set();
 let counterForLocalStorage = 0;
 tabsSection.addEventListener('click', (e) => {
 	if (e.target.classList.contains('app__weather-tab')) {
@@ -59,8 +61,8 @@ inputValue.addEventListener('input', () => {
 	localStorage.setItem('inputStorage', inputValue.value);
 })
 // Trying to fetch data 
-document.addEventListener('DOMContentLoaded', () => {
-
+document.addEventListener('DOMContentLoaded', (e) => {
+	e.preventDefault();
 	fetch('http://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&appid=f660a2fb1e4bad108d6160b7f58c555f')
 		.then(response => response.json())
 		.then(data => {
@@ -82,4 +84,58 @@ document.addEventListener('DOMContentLoaded', () => {
 				localStorage.removeItem('inputStorage');
 			}
 		});
+	// After DOM Content loaded get info for set
+	for (let key in localStorage) {
+		if (!localStorage.hasOwnProperty(key) || key === "inputStorage") {
+			continue;
+		}
+		counterForLocalStorage++;
+		set.add(localStorage.getItem(key));
+	}
+	for (let value of set) {
+		addedLocations.insertAdjacentHTML('beforeend', `<p class="app__favourite-city">${value}
+		 <svg class="app__favourite-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/></svg>
+		 </p>`);
+	};
+})
+// Sort out keys in localStorage
+favouriteCity.addEventListener('click', () => {
+	if (weatherCityFavourite.textContent !== "undefined") {
+		counterForLocalStorage++;
+		localStorage.setItem(`${counterForLocalStorage}`, `${weatherCityFavourite.textContent}`);
+		alert(localStorage.getItem('1'));
+		alert(counterForLocalStorage);
+	}
+	for (let key in localStorage) {
+		if (!localStorage.hasOwnProperty(key) || key === "inputStorage") {
+			continue;
+		}
+		counterForLocalStorage++;
+		set.add(localStorage.getItem(key));
+		console.log(set);
+	}
+})
+addedLocations.addEventListener('click', (e) => {
+	if (e.target.classList.contains('app__favourite-city')) {
+		e.preventDefault();
+		fetch('http://api.openweathermap.org/data/2.5/weather?q=' + e.target.textContent + '&appid=f660a2fb1e4bad108d6160b7f58c555f')
+			.then(response => response.json())
+			.then(data => {
+				if (data.message !== "city not found" || data.message !== "Nothing to geocode") {
+					console.log(data);
+					weatherCityArray.forEach(item => {
+						item.textContent = `${data.name}`
+					})
+					weatherDigitsValue.textContent = `${Math.round(data.main.temp) - 273}°C`
+					weatherInformation.textContent = `Temperature: ${Math.round(data.main.temp - 273)}°C`;
+					weatherFeelsLike.textContent = `Feels Like: ${Math.round(data.main.feels_like - 273)}°C`;
+					weather.textContent = `Weather: ${data.weather[0].main}`;
+					weatherSunrise.textContent = `Sunrise: ${Math.floor((data.sys.sunrise / (1000 * 60 * 60)) % 24)}:${Math.floor((data.sys.sunrise / (1000 * 60)) % 60)} `;
+					weatherSunset.textContent = `Sunset: ${Math.floor((data.sys.sunset / (1000 * 60 * 60)) % 24)}:${Math.floor((data.sys.sunset / (1000 * 60)) % 60)} `;
+				}
+			});
+	}
+	// if (e.target.classList.contains('app__favourite-icon')) {
+	// 	alert('DELETE')
+	// }
 })
